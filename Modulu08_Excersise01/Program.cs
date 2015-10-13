178acafb361d2e5091504b75eaf84b57f8d44b79
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2015-10-13 17:59:15 -0500

    Commit

--- Modulu08_Excersise01/Program.cs
+++ Modulu08_Excersise01/Program.cs
@@ -9,10 +9,15 @@ using System.Threading.Tasks;
 
 namespace Modulu08_Excersise01
 {
-    class MainProgram
+    class Program
     {
         static void Main(string[] args)
         {
+            double total = 0;
+            
+            Shape shape01 = new Shape();
+            total = shape01.Area(4.0, 5.0);
+            Console.WriteLine("The area of is: {0}.", total);
 
             Console.ReadKey();
         }
