6f59d23612139d7e204bf13cf2b08c5184f677af
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2015-10-13 18:54:35 -0500

    FINAL COMMIT

--- Modulu08_Excersise01/Program.cs
+++ Modulu08_Excersise01/Program.cs
@@ -9,17 +9,31 @@ using System.Threading.Tasks;
 
 namespace Modulu08_Excersise01
 {
+    public delegate int AddDelagate(int num1, int num2);
+
     class Program
     {
         static void Main(string[] args)
         {
             double total = 0;
-            
+            int result = 0;
+
             Shape shape01 = new Shape();
             total = shape01.Area(4.0, 5.0);
             Console.WriteLine("The area of is: {0}.", total);
 
+            AddDelagate ad01 = new AddDelagate(Add);
+            result = ad01(1, 2);
+            Console.WriteLine("The result of adding 1 and 2, via a delagate, is: {0}", result);
+ 
+
+
             Console.ReadKey();
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
         }
     }
 }
--- Modulu08_Excersise01/Rectangle.cs
+++ Modulu08_Excersise01/Rectangle.cs
@@ -8,6 +8,6 @@ namespace Modulu08_Excersise01
 {
     class Rectangle : Shape
     {
-
+ 
     }
 }
